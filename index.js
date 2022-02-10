e240e9d6ababe385495e91d703f6d959ce5ed9c0
Your Name <joshlpaige@gmail.com>
2022-02-10 17:52:46 -0500

    adding process env

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const db = require("./db");
 const express = require("express");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+
 const server = express();
 
 server.listen(PORT, () => {
